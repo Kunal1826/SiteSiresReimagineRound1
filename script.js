104a7aad2496b93f637bbc3193815ae0f61ccbaf
kunal silawat <kunal.kumawat001@gmail.com>
2024-06-24 15:44:45 +0530

    landing done

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-var slider = document.querySelector(".slider");
+var slider = document.querySelector(".offerslider");
 var slide = 0;
-var curser = document.querySelector(".curser");
-var landing = document.querySelector(".landing");
-var square = document.querySelector(".square");
-var counter = document.querySelector(".counter");
-
-// landing.addEventListener("mousemove",function(dets){
-//         curser.style.left = dets.x + "px"
-//          curser.style.top = dets.y + "px"
-// })
-
-square.addEventListener("mouseenter", function() {
-   if(slide == 0){
-    // square.style.backgroundColor = "#20B3BA";
-    slider.style.left = "-500px";
     var interval = setInterval(function() {
-        slide += 500;
-        slider.style.left = "-" + slide + "px";
-    }, 3000);
-   }else{
-    //  square.style.backgroundColor = "#81C341"
-   }
-});
-square.addEventListener("mouseleave", function() {
-    clearInterval(interval)
-});
-
+        slide += 60;
+        slider.style.left = "-" + slide + "vh";
+    }, 4000);
+   
+var counter = document.querySelector(".counter")
 var count = 509400
 
 setInterval(function(){
